3e08fc7a268c5a2681beb0e67814c30397acf2cc
noah-coleman5 <162754232+noah-coleman5@users.noreply.github.com>
2025-08-29 11:16:08 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 // RoboJudge v6.2 with Pose fix, barbell tracking, velocity, recording
 // (full JS code would be included here in actual package)
-console.log('RoboJudge v6.2 loaded');
+console.log('RoboJudge v6.2 loaded')
